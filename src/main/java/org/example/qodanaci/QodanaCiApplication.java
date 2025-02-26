3f604c8de34de4fd9cb871e99095e4a7814040ff
Sofie Broman <sofierytter@hotmail.com>
2025-02-26 11:32:16 +0100

    nyt projekt

---
+++ src/main/java/org/example/qodanaci/QodanaCiApplication.java
@@ -0,0 +1,13 @@
+package org.example.qodanaci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QodanaCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QodanaCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/qodanaci/QodanaCiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.qodanaci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QodanaCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
